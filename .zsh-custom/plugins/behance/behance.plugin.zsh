d31c0015f69fb581a3acfac218f867f4fc92f0ed
Steve Davis <steve@celery.club>
2018-11-16 15:15:15 -0500

    Add more PHP aliases to behance plugin

--- .zsh-custom/plugins/behance/behance.plugin.zsh
+++ .zsh-custom/plugins/behance/behance.plugin.zsh
@@ -10,6 +10,9 @@ alias fl=flotilla
 
 # PHP aliases
 alias phpu="./vendor/bin/phpunit"
+alias phpmd="./vendor/bin/phpmd `git diff --name-only --diff-filter=d upstream/master..HEAD | grep .php | tr '\n' ',' | rev | cut -c 2- | rev` text ./vendor/behance/phpmd-rules/phpmd.xml.dist"
+alias phpcs="./vendor/bin/phpcs --standard=./vendor/behance/php-sniffs/Behance/ruleset.xml -s -n `git diff --diff-filter=d --name-only upstream/master..HEAD | grep .php | tr '\n' ' ' | rev | cut -c 2- | rev`"
+alias phpcbf="./vendor/bin/phpcbf --standard=./vendor/behance/php-sniffs/Behance/ruleset.xml `git diff --diff-filter=d --name-only upstream/master..HEAD | grep .php | tr '\n' ' ' | rev | cut -c 2- | rev`"
 alias php-sniff="git diff upstream/master --name-only --pretty=format: --diff-filter=AM | grep '.php$' | xargs ./vendor/bin/phpcs --standard=./vendor/behance/php-sniffs/Behance/"
 alias php-fix="git diff upstream/master --name-only --pretty=format: --diff-filter=AM | grep '.php$' | xargs ./vendor/bin/phpcbf --standard=./vendor/behance/php-sniffs/Behance/ruleset.xml"
 
