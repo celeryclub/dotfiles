c46607a94a2b37dc7030ace603628fd5844cb6c3
Steve Davis <steve@celery.club>
2019-12-02 19:24:06 -0500

    Improve performance of nvm plugin

--- .zsh-custom/plugins/nvm/nvm.plugin.zsh
+++ .zsh-custom/plugins/nvm/nvm.plugin.zsh
@@ -1,5 +1,15 @@
-# Set NVM_DIR if it isn't already defined
-[[ -z "$NVM_DIR" ]] && export NVM_DIR="$HOME/.nvm"
+# Based on https://www.reddit.com/r/node/comments/4tg5jg/lazy_load_nvm_for_faster_shell_start/d5ib9fs/
 
-# Load nvm if it exists
-[[ -f "$(brew --prefix nvm)/nvm.sh" ]] && source "$(brew --prefix nvm)/nvm.sh"
+declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+
+NODE_GLOBALS+=("node")
+NODE_GLOBALS+=("nvm")
+
+load_nvm() {
+  [[ -z "$NVM_DIR" ]] && export NVM_DIR="$HOME/.nvm"
+  [[ -f "$(brew --prefix nvm)/nvm.sh" ]] && source "$(brew --prefix nvm)/nvm.sh"
+}
+
+for cmd in "${NODE_GLOBALS[@]}"; do
+  eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
+done
